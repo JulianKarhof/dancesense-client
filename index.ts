c49c3bbbd54cb900c4bb64fcacf186de18d18c84
FLuplow <felix.luplow@code.berlin>
2023-06-05 20:46:26 +0200

    add recursion cool

--- index.ts
+++ index.ts
@@ -1,48 +1,59 @@
-import { SerialPort } from 'serialport';
-import { ReadlineParser } from '@serialport/parser-readline';
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
 import { keyboard, Key } from "@nut-tree/nut-js";
 
 async function main() {
   keyboard.config.autoDelayMs = 0;
 
   const ports = await SerialPort.list();
+  const padPath = ports.find((port) => port.productId === "ea60")?.path;
+  if (!padPath) {
+    main();
+  } else {
+    const port = new SerialPort(
+      {
+        path: padPath,
+        baudRate: 115200,
+      },
+      function (err) {
+        if (err) {
+          return console.log("Error: ", err.message);
+        }
+      }
+    );
 
-  const port = new SerialPort({
-    path: ports.find((port) => port.productId === 'ea60')?.path ?? '',
-    baudRate: 115200,
-  }, function(err) {
-    if (err) {
-      return console.log('Error: ', err.message)
-    }
-  })
+    const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
 
-  const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-
-  port.on("open", function() {
-    console.log("-- Connection opened --");
-    parser.on("data", async function(data: string) {
-      console.log(data);
-      handleKeys(data);
+    port.on("open", function () {
+      console.log("-- Connection opened --");
+      parser.on("data", async function (data: string) {
+        console.log(data);
+        handleKeys(data);
+      });
     });
-  });
+
+    port.on("close", () => {
+      console.log("-- Connection closed --");
+      main();
+    });
+  }
 
   let old = [false, false, false, false];
 
   async function handleKeys(data: string) {
-    const values: Array<boolean> = data.split(" ").map((d) => d === '1')
+    const values: Array<boolean> = data.split(" ").map((d) => d === "1");
 
-    if (values[0]) keyboard.pressKey(Key.Left)
-    else if (old[0] != values[0]) keyboard.releaseKey(Key.Left)
-    if (values[1]) keyboard.pressKey(Key.Up)
-    else if (old[1] != values[1]) keyboard.releaseKey(Key.Up)
-    if (values[2]) keyboard.pressKey(Key.Right)
-    else if (old[2] != values[2]) keyboard.releaseKey(Key.Right)
-    if (values[3]) keyboard.pressKey(Key.Down)
-    else if (old[3] != values[3]) keyboard.releaseKey(Key.Down)
+    if (values[0]) keyboard.pressKey(Key.Left);
+    else if (old[0] != values[0]) keyboard.releaseKey(Key.Left);
+    if (values[1]) keyboard.pressKey(Key.Up);
+    else if (old[1] != values[1]) keyboard.releaseKey(Key.Up);
+    if (values[2]) keyboard.pressKey(Key.Right);
+    else if (old[2] != values[2]) keyboard.releaseKey(Key.Right);
+    if (values[3]) keyboard.pressKey(Key.Down);
+    else if (old[3] != values[3]) keyboard.releaseKey(Key.Down);
 
     old = values;
   }
 }
-
 
 main();
